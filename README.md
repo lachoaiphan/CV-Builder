499b4220e2682481cf90eb4d9d8cd246033e5e72
Lac Phan <lhphan@ucdavis.edu>
2021-01-26 18:04:42 -0800

    Initial app version

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# CV Builder
+# Resume Builder
 
 A web application that allows the user to build their own CV with an existing template. Uses React Components to keep track of specific class components' state.
 
-**[Link]()** supported by Github Pages.
+**[https://lachoaiphan.github.io/CV-Builder/]()** supported by Github Pages.
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-  import React, { Component } from 'react';
+import React, { Component } from 'react';
 import Contact from './components/Contact.js';
 import Education from './components/Education.js';
 import Experience from './components/Experience.js';
@@ -6,18 +6,18 @@ import './styles/App.css';
 
 class App extends Component {
   render () {
-    return (
-        <div id="app">
-            <div id="title-container" className="mar-45">
-                <h1>CV Builder</h1>
-            </div>
-            <div id="resume-builder" className="slideIn">
-                <Contact />
-                <Experience />
-                <Education />
-            </div>
-        </div>
-    )
+      return (
+          <div id="app">
+              <div id="title-container" className="mar-45">
+                  <h1>Resume Builder</h1>
+              </div>
+              <div id="resume-builder" className="slideIn">
+                  <Contact />
+                  <Experience />
+                  <Education />
+              </div>
+          </div>
+      )
   }
 }
 
